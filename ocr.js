93a744bcc05411d23734cd588340d69cda67dcc4
Rohan Pai <me@rohanpai.com>
2014-02-05 23:02:48 -0800

    functional key-based image encryption

--- ocr.js
+++ ocr.js
@@ -22,12 +22,22 @@ var encryptImg = function(){
             var encryptedCanvas = document.createElement("canvas");
             encryptedCanvas.width = imgCanvas.width;
             encryptedCanvas.height = imgCanvas.height;
-        
+            
+            var key = Math.random() * 1000;
+
             var encryptedCtx = encryptedCanvas.getContext("2d");
             var imgCtx = imgCanvas.getContext("2d");
             for(var i = 0; i < imgCanvas.height; i++){
                 for(var j = 0; j < imgCanvas.width; j++){
                     var imgPixel = imgCtx.getImageData(i,j,1,1).data;
+
+                    imgPixel[0] = (imgPixel[0] * key) % 255;
+                    imgPixel[1] = (imgPixel[1] * key) % 255;
+                    imgPixel[2] = (imgPixel[2] * key) % 255;
+                    imgPixel[3] = (imgPixel[3] * key) % 255;
+
+                    //imgPixel[1] += -1;
+
                     encryptedCtx.fillStyle = "rgb("+imgPixel[0]+","+imgPixel[1]+","+imgPixel[2]+")"; 
                     //if(i == 1) console.log( "rgb("+imgPixel[0]+","+imgPixel[1]+","+imgPixel[2]+)" );
                     //encryptedCtx.fillStyle = "rgb(164,188,212)";
@@ -35,7 +45,7 @@ var encryptImg = function(){
                     
                 }         
             }
-            return encryptedCanvas;
+            return convertCanvasToImage(encryptedCanvas);
          }
 
     }
